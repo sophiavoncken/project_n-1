2cae240c31703534ccee95baae427607904b7b70
sophiavoncken <vonckands11@gmail.com>
2024-06-17 16:20:29 -0300

    Add files via upload

---
+++ sophia/site_php/formulario.php
@@ -0,0 +1,6 @@
+<?php
+    echo("Nome: ".$_POST["txt_nome"]. "<br>"); 
+    echo("Email: ".$_POST["txt_email"]. "<br>"); 
+    echo("Telefone: ".$_POST["txt_telefone"]. "<br>"); 
+    echo("<a href=\"http://127.0.0.1:5500/contato.html\" >Voltar</a>");
+?>
