88953b2e6cc9e887025107e66ec73cba34a6add2
sophiavoncken <vonckands11@gmail.com>
2024-11-26 15:50:10 -0300

    Delete projs.js directory

--- projs.js/js_dom.js
+++
@@ -1,9 +0,0 @@
-document.title="Usando DOM: Alterado!!";
-document.write("Navegador - appName; "+navigator.appName+"<br>");
-document.write("Navegador - platform: "+navigator.platform+"<br>");
-document.write("Navegador - appCodeName: "+navigator.appCodeName+"<br>");
-document.write("Navegador - product: "+navigator.product+"<br>");
-document.write("Navegador - appVersion: "+navigator.appVersion+"<br>");
-document.write("Navegador - userAgent: "+navigator.userAgent+"<br>");
-document.write("Navegador - onLine: "+navigator.onLine+"<br>");
-document.write("Navegador - language: "+navigator.language+"<br>");
